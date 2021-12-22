83f0102464aac3edfad65fe5784f38310ed0cce2
mustafa <mltm0204@gmail.com>
2021-12-22 12:42:40 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(()=>{
+    const lastIndex = people.length - 1;
+    if(index < 0 ){
+      setIndex(lastIndex);
+    }
+    if(index > lastIndex){
+      setIndex(0)
+    }
+  },[index, people])
+
+  useEffect(()=>{
+   let slider = setInterval(()=> {
+      setIndex(index + 1);
+    }, 3000);
+    return()=> clearInterval(slider);
+  }, [index])
+
   return (
     <section className='section'>
         <div className="title">
@@ -22,6 +39,10 @@ function App() {
                 if (personIndex === index){
                   position = 'activeSlide';
                 }
+                if(personIndex === index - 1 || 
+                  (index === 0 && personIndex === people.length - 1)){
+                    position = 'lastSlide';
+                }
 
                 return (
                   <article className={position} key={id}>
@@ -33,10 +54,10 @@ function App() {
                   </article>
                 )
             })}
-            <button className='prev'>
+            <button className='prev' onClick={()=> setIndex(index - 1)}>
               <FiChevronLeft/>
             </button>
-            <button className='next'>
+            <button className='next' onClick={()=> setIndex(index + 1)}>
               <FiChevronRight/>
             </button>
         </div>
